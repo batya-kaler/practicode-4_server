a0accd782601922266be53cdaac88eed4a9db13d
Batya <batyakaler@gmail.com>
2025-03-18 15:28:09 +0200

    update a service

--- Program.cs
+++ Program.cs
@@ -1,71 +1,7 @@
 
-// using TodoApi; 
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.OpenApi.Models; 
-// var builder = WebApplication.CreateBuilder(args);
-// builder.Services.AddDbContext<ToDoDbContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"), new MySqlServerVersion(new Version(8, 0, 25))));
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen(c =>
-// {
-//     c.SwaggerDoc("v1", new OpenApiInfo
-//     {
-//         Title = "ToDo API",
-//         Version = "v1",
-//         Description = "API לניהול משימות"
-//     });
-// });
-
-// builder.Services.AddCors(options =>
-// {
-//       options.AddPolicy("AllowAll", policy =>
-//     {
-//         policy.AllowAnyOrigin()
-//               .AllowAnyMethod()
-//               .AllowAnyHeader();
-//     });
-// });
-
-// var app = builder.Build();
-//     app.UseSwagger();
-//     app.UseSwaggerUI(c =>
-//     {
-//         c.SwaggerEndpoint("/swagger/v1/swagger.json", "ToDo API V1");
-//     });
-// app.UseCors("AllowAll");
-// app.MapGet("/items", async (ToDoDbContext db) => await db.Items.ToListAsync());
-// app.MapPost("/", async (ToDoDbContext db,string name) =>
-// {
-//     var item = new Item { Name = name, Iscomplete = false };
-//     await db.Items.AddAsync(item);
-//     await db.SaveChangesAsync();
-//     return await db.Items.ToListAsync();
-// });
-// app.MapDelete("{id}", async (ToDoDbContext db, int id) =>
-// {
-//     var item = await db.Items.FindAsync(id);
-//     if (item == null)
-//         return Results.NotFound();
-//     db.Remove(item);
-//     await db.SaveChangesAsync();
-//     return Results.Ok();
-// });
-// app.MapPatch("{id}",async(ToDoDbContext db,int id,bool IsComplete) =>{
-
-//    var find=await db.Items.FindAsync(id);
-//     if(find==null)
-//     return Results.NotFound();
-//    find.Iscomplete = IsComplete;
-//     await db.SaveChangesAsync();
-// return Results.Ok();
-// });
-// app.MapGet("",()=>"success!!!!!");
-// app.Run();
-
-
-using TodoApi;
+using TodoApi; 
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-
+using Microsoft.OpenApi.Models; 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ToDoDbContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"), new MySqlServerVersion(new Version(8, 0, 25))));
 builder.Services.AddEndpointsApiExplorer();
@@ -81,7 +17,7 @@ builder.Services.AddSwaggerGen(c =>
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", policy =>
+      options.AddPolicy("AllowAll", policy =>
     {
         policy.AllowAnyOrigin()
               .AllowAnyMethod()
@@ -90,27 +26,21 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ToDo API V1");
-});
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ToDo API V1");
+    });
 app.UseCors("AllowAll");
-
-app.MapGet("/", () => "success!!!!!");
-
 app.MapGet("/items", async (ToDoDbContext db) => await db.Items.ToListAsync());
-
-app.MapPost("/", async (ToDoDbContext db, HttpRequest request) =>
+app.MapPost("/", async (ToDoDbContext db,string name) =>
 {
-    var name = request.Query["name"].ToString();
     var item = new Item { Name = name, Iscomplete = false };
     await db.Items.AddAsync(item);
     await db.SaveChangesAsync();
     return await db.Items.ToListAsync();
 });
-
-app.MapDelete("/{id}", async (ToDoDbContext db, int id) =>
+app.MapDelete("{id}", async (ToDoDbContext db, int id) =>
 {
     var item = await db.Items.FindAsync(id);
     if (item == null)
@@ -119,16 +49,16 @@ app.MapDelete("/{id}", async (ToDoDbContext db, int id) =>
     await db.SaveChangesAsync();
     return Results.Ok();
 });
+app.MapPatch("{id}",async(ToDoDbContext db,int id,bool IsComplete) =>{
 
-app.MapPatch("/{id}", async (ToDoDbContext db, int id, HttpRequest request) =>
-{
-    var isComplete = bool.Parse(request.Query["IsComplete"]);
-    var find = await db.Items.FindAsync(id);
-    if (find == null)
-        return Results.NotFound();
-    find.Iscomplete = isComplete;
+   var find=await db.Items.FindAsync(id);
+    if(find==null)
+    return Results.NotFound();
+   find.Iscomplete = IsComplete;
     await db.SaveChangesAsync();
-    return Results.Ok();
+return Results.Ok();
 });
-
+app.MapGet("/",()=>"success!!!!!");
 app.Run();
+
+
